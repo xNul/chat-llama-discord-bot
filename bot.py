386dbc5772e0806c8dcdd063a29626cf74802047
mercm8 <37743453+mercm8@users.noreply.github.com>
2023-05-09 06:05:38 +0200

    Update bot.py
    clear_chat_log() needs the mode, using default

--- bot.py
+++ bot.py
@@ -367,7 +367,7 @@ async def reset(ctx, prompt_new=prompt, your_name_new=your_name, llamas_name_new
     reply_count = 0
     
     shared.stop_everything = True
-    clear_chat_log(your_name, llamas_name, "", "")
+    clear_chat_log(your_name, llamas_name, "", "","cai-chat")
     
     logging.info("conversation reset: {'replies': " + str(reply_count) + ", 'your_name': '" + your_name + "', 'llamas_name': '" + llamas_name + "', 'prompt': '" + prompt + "'}")
     reset_embed.timestamp = datetime.now() - timedelta(hours=3)
